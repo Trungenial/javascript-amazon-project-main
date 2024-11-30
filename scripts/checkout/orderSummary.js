baa579144230728007ca21c02171b35aab32677e
Tran Sy Trung <transytrung0752.work@gmail.com>
2024-11-30 09:10:21 +0700

    Convert products into classes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -44,10 +44,8 @@ export function renderOrderSummary() {
         <div class="cart-item-details">
           <div class="product-name">
             ${matchingProduct.name}
-          </div>
-          <div class="product-price">$${formatCurrency(
-            matchingProduct.priceCents
-          )}</div>
+          </div>  
+          <div class="product-price">${matchingProduct.getPrice()}</div>
           <div class="product-quantity js-product-quantity-${
             matchingProduct.id
           }">
